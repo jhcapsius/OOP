01c6fffd5eacbf9edd3bf1bf7dce228d35350884
Jan-Henrik <jan.henrik.capsius@googlemail.com>
2021-12-07 19:42:33 +0100

    testclass for Zahlenkombinationen.java implemented

--- Sheet8/Test/ZahlenkombinationenTest.java
+++ Sheet8/Test/ZahlenkombinationenTest.java
@@ -1,11 +1,28 @@
+/**
+ *Testclass for Zahlenkombinationen.java
+ *
+ * @author Jan-Henrik Capsius
+ * @version 1.0
+ */
+
 public class ZahlenkombinationenTest {
     public static void main(String[] args) {
         Zahlenkombinationen zahlenkombinationen = new Zahlenkombinationen();
-        zahlenkombinationen.initArray(7, 4, 2);
-        zahlenkombinationen.printArr();
+        
+        //test when array is not initialized
         zahlenkombinationen.loeschen();
-        zahlenkombinationen.printHorizontal();
-        zahlenkombinationen.printVertical();
-        zahlenkombinationen.printDiagonal();
+        zahlenkombinationen.anzeigen();
+        zahlenkombinationen.auffuellen();
+        System.out.println();
+
+        //test fill array with random numbers between 1 and 4 and delete number chains of three or more.
+        zahlenkombinationen.initArray(5, 7, 4);
+        zahlenkombinationen.anzeigen();
+        zahlenkombinationen.loeschen();
+        zahlenkombinationen.anzeigen();
+
+        //test fill the empty spaces when there are remaining values
+        zahlenkombinationen.auffuellen();
+        zahlenkombinationen.anzeigen();
     }
 }
