a0c7a768e30179a5d65acdf50340b95b7ba260d8
Jan-Henrik <jan.henrik.capsius@googlemail.com>
2021-12-08 03:34:32 +0100

    Comments added

--- Sheet8/src/Warenkorb.java
+++ Sheet8/src/Warenkorb.java
@@ -1,36 +1,59 @@
+/**
+ * With the Shopping Cart class, items can be added and removed using the addItem and removeItem methods. In addition, the contents of the shopping cart and the total price can be printed using the printContent method.
+ * 
+ * @author Jan-Henrik Capsius
+ * @version 1.0
+ */
+
 import java.util.ArrayList;
 
 public class Warenkorb {
     private ArrayList<Artikel> artikelListe = new ArrayList<>();
     private double totalPrice;
 
+    /**
+     * Method to add articles
+     * @param artikel Article to be added
+     */
     public void addItem(Artikel artikel){
         this.artikelListe.add(artikel);
     }
 
-    public void removeItem(int artikelNummer){
-        for(int i = 0; i < this.artikelListe.size(); i++){
-            if(this.artikelListe.get(i).getArtikelNummer() == artikelNummer){
-                this.artikelListe.remove(i);
-                i = this.artikelListe.size();
-            }
+    /**
+     * Method to remove articles
+     * @param index Article to be removed
+     */
+    public void removeItem(int index){
+        try{
+            this.artikelListe.remove(index);
+        }catch(IndexOutOfBoundsException e){
+            System.out.println("An der Stelle gibt es kein Item.\n");
         }
+        
     }
 
+    /**
+     * prints the content of the shopping card
+     */
     public void printContent(){
         for(Artikel artikel : artikelListe){
-            System.out.println(artikel.toString() + artikel.getInfo());
+            System.out.printf(artikel.toString() + " : %.2f Euro\n", artikel.getPreis());
         }
+
+        printTotalPrice();
     }
 
-    public void printTotalPrice(){
+    /**
+     * calculates the total price
+     */
+    private void printTotalPrice(){
         if(this.totalPrice != 0){
             this.totalPrice = 0;
         }
         for (Artikel artikel : artikelListe) {
             totalPrice += artikel.getPreis();
         }
-        System.out.printf("Gesamtpreis: %.2f Euro", totalPrice);
+        System.out.printf("\nGesamtpreis: %.2f Euro\n\n", totalPrice);
     } 
     
 }
