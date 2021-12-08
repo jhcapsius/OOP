d161231082f6f284ec9a822831d6298a8c87d220
Jan-Henrik <jan.henrik.capsius@googlemail.com>
2021-12-08 03:24:04 +0100

    comments added

--- Sheet8/src/DVD.java
+++ Sheet8/src/DVD.java
@@ -1,20 +1,32 @@
+/**
+ * The DVD class inherits from the article class and additionally has the class attributes hours, minutes, movie title and the constant dvd. The attributes can be set via the constructor and returned as a string with the toString method.
+ */
+
 public class DVD extends Artikel{
     private int minuten, stunden;
     private String filmTitel;
-    private final String dvd = "DVD";
+    private static final String dvd = "DVD";
 
     
-
+    /**
+     * sets the attributs via constructor
+     * @param filmTitel movietitle
+     * @param dauer duration of the movie
+     * @param artikelNummer article number
+     * @param nettoPreis net price
+     */
     DVD(String filmTitel, int dauer, int artikelNummer, double nettoPreis){
         super(artikelNummer, (nettoPreis * 1.19));
         this.filmTitel = filmTitel;
         this.stunden = dauer / 60;
         this.minuten = dauer % 60;
     }
-
+    /**
+     * //returns the attributes as string
+     */
     @Override
     public String toString() {
-        return  this.minuten >= 10 ? this.dvd + " - " + this.filmTitel + " (" + this.stunden + ":" + this.minuten + ") " : this.dvd + " - " + this.filmTitel + " (" + this.stunden + ":" + "0" + this.minuten + ") ";
+        return  this.minuten >= 10 ? dvd + " - " + this.filmTitel + " (" + this.stunden + ":" + this.minuten + ") " : dvd + " - " + this.filmTitel + " (" + this.stunden + ":" + "0" + this.minuten + ") ";
     }
     
 }
--- Sheet8/src/Kleidung.java
+++ Sheet8/src/Kleidung.java
@@ -1,7 +1,20 @@
+/**
+ * The Clothing class inherits from the article class and additionally has the class attributes typ, colour, size and the constant clothes. The attributes can be set via the constructor and returned as a string with the toString method.
+ */
+
 public class Kleidung extends Artikel{
     private String typ, farbe, groesse;
-    private final String kleidung = "Kleidung";
+    private static final String kleidung = "Kleidung";
 
+
+    /**
+     * sets the attributs via constructor
+     * @param typ type of clothing
+     * @param farbe type of colour
+     * @param groesse size of the clothing
+     * @param artikelNummer article number
+     * @param nettoPreis net price
+     */
     Kleidung(String typ, String farbe, String groesse, int artikelNummer, double nettoPreis){
         super(artikelNummer, (nettoPreis * 1.12));
         this.typ = typ;
@@ -9,9 +22,12 @@ public class Kleidung extends Artikel{
         this.groesse = groesse;
     }
 
+    /**
+     * returns the attributes as string
+     */
     @Override
     public String toString() {
-        return this.kleidung + " - " + typ + ", " + farbe + " (" + groesse.toUpperCase() + ") "; 
+        return kleidung + " - " + typ + ", " + farbe + " (" + groesse.toUpperCase() + ") "; 
     }
     
 }
