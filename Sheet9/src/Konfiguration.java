bea67c6a61b0e049f3a92ed41d49a0495d1f0e98
Jan-Henrik <jan.henrik.capsius@googlemail.com>
2021-12-11 01:51:05 +0100

    Documentation added

--- Sheet9/src/Konfiguration.java
+++ Sheet9/src/Konfiguration.java
@@ -1,19 +1,38 @@
+/**
+ * Example class how to implement the Singleton-Pattern
+ * 
+ * @author Jan-Henrik Capsius
+ * @version 1.0
+ */
+
+
 import java.util.HashMap;
 
 public class Konfiguration {
+
+    //private static final attrinute that contains the only instance of Konfiguration
     private static final Konfiguration singletonKonfiguation = new Konfiguration();
 
     private HashMap<String, String> keyValuePaare;
 
+    //Private constructor to make sure, that no objects can be created.
     private Konfiguration(){
         System.out.println("Konfiguration wird erstellt.");
         this.keyValuePaare = new HashMap<>();
     }
 
+    /**
+     * static factory method for obtaining the instance
+     * @return instance of Konfiguration
+     */
     public static Konfiguration getKonfiguration(){
         return singletonKonfiguation;
     }
 
+    /**
+     * geter for the hashmap
+     * @return hashmap
+     */
     public HashMap<String, String> getWerte(){
         return this.keyValuePaare;
     }
@@ -22,6 +41,11 @@ public class Konfiguration {
 
     }
 
+    /**
+     * setter for the hashmap
+     * @param key key of the hashmap entry 
+     * @param wert value of the hasmap entry
+     */
     public void setWert(String key, String wert){
         this.keyValuePaare.put(key, wert);
     }
