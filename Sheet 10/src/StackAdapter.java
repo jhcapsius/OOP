52dbb1d97181c97d229a80385c603607bc817db4
Jan-Henrik <jan.henrik.capsius@googlemail.com>
2021-12-16 08:19:25 +0100

    Stack adapter implemented

---
+++ Sheet 10/src/StackAdapter.java
@@ -0,0 +1,29 @@
+public class StackAdapter<T> implements IStackAdapter<T> {
+    Stack<T> stack;
+
+    StackAdapter(){
+        this.stack = new Stack<>();
+    }
+
+    @Override
+    public boolean empty() {
+        return this.stack.empty();
+    }
+
+    @Override
+    public T push(T item) {
+        return this.stack.push(item);
+    }
+
+    @Override
+    public T pop() {
+        return this.stack.pop();
+    }
+
+    @Override
+    public T peek() {
+        return this.stack.peek();
+    }
+
+    
+}
